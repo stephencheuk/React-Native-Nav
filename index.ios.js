04741b9710f033c325f448bb56e4c74d4296f371
stephencheuk <cheuklt@gmail.com>
2017-02-01 13:35:03 +0800

    IOS - use NavigatorIOS

--- index.ios.js
+++ index.ios.js
@@ -1,46 +1,18 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  Navigator
+  StyleSheet,
+  Text,
+  View,
+  NavigatorIOS,
+  TouchableHighlight
 } from 'react-native';
 
 import MyScene from './MyScene';
 
 class nav02 extends Component {
 
-//  ver 1
-//  render() {
-//    return (
-//      <MyScene />
-//    )
-//  }
-//  ver 2
-  render() {
     return (
-      <Navigator
-        initialRoute={{ title: 'My Initial Scene', index: 0 }}
-        renderScene={(route, navigator) => 
-          <MyScene
-            title={route.title}
-
-            // Function to call when a new scene should be displayed
-            onForward={() => {    
-              const nextIndex = route.index + 1;
-              navigator.push({
-                title: 'Scene ' + nextIndex,
-                index: nextIndex,
-              });
-            }}
-
-            onBack={() => {
-              if (route.index > 0) {
-                navigator.pop();
-              }
-            }}
-
-          />
-        }
-      />
     );
   }
 }
