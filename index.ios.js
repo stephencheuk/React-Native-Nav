ec76777ffd6bfb8773d934c9bbd7d1563692c9d3
stephencheuk <cheuklt@gmail.com>
2017-02-01 13:40:36 +0800

    update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -8,13 +8,77 @@ import {
   TouchableHighlight
 } from 'react-native';
 
-import MyScene from './MyScene';
-
 class nav02 extends Component {
 
+  onRightButtonPress: function() {
+    this.refs.nav.push({
+      title: 'From Right',
+      component: ForRightScene
+    })
+  },
+
+  render () {
     return (
+      <NavigatorIOS ref="nav" style={styles.container} initialRoute={{
+          component: HomeScene,
+          title: 'NavigatorIOS Demo',
+          rightButtonTitle: 'MORE!',
+          onRightButtonPress: this.onRightButtonPress
+      }} />
     );
   }
+
 }
+
+var HomeScene = React.createClass({
+    onPress() {
+        this.props.navigator.push({
+            title: 'From TouchableHighlight',
+            component: ForTouchScene
+        });
+    },
+
+    render() {
+        return (
+            <View style={[styles.scene, {backgroundColor: '#DAF6FF'}]}>
+                <TouchableHighlight onPress={this.onPress}>
+                    <Text>Welcome to the NavigatorIOS Demo. Press here!</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+});
+
+var ForRightScene = React.createClass({
+    render() {
+        return (
+            <View style={[styles.scene, {backgroundColor: '#FFF1E8'}]}>
+                <Text>You came here from the "right" button!</Text>
+            </View>
+        );
+    }
+});
+
+
+var ForTouchScene = React.createClass({
+    render() {
+        return (
+            <View style={[styles.scene, {backgroundColor: '#ECF6E8'}]}>
+                <Text>You came here from the TouchableHighlight!</Text>
+            </View>
+        );
+    }
+});
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    scene: {
+        padding: 10,
+        paddingTop: 74,
+        flex: 1
+    }
+});
 
 AppRegistry.registerComponent('nav02', () => nav02);
