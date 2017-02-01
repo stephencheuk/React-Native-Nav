450772cce043f1dce6cbec752b5cea968c62ec40
stephencheuk <cheuklt@gmail.com>
2017-02-01 12:12:58 +0800

    update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  Navigator
 } from 'react-native';
 
-export default class nav02 extends Component {
+import MyScene from './MyScene';
+
+class nav02 extends Component {
+
+//  ver 1
+//  render() {
+//    return (
+//      <MyScene />
+//    )
+//  }
+//  ver 2
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={{ title: 'My Initial Scene', index: 0 }}
+        renderScene={(route, navigator) => 
+          <MyScene
+            title={route.title}
+
+            // Function to call when a new scene should be displayed
+            onForward={() => {    
+              const nextIndex = route.index + 1;
+              navigator.push({
+                title: 'Scene ' + nextIndex,
+                index: nextIndex,
+              });
+            }}
+
+            onBack={() => {
+              if (route.index > 0) {
+                navigator.pop();
+              }
+            }}
+
+          />
+        }
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('nav02', () => nav02);
